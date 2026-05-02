d66df55bb744cfdd8c6218cce4641290830bef1d
lagunamanuel <lagunamanuel8@gmail.com>
2026-05-02 11:18:21 +0200

    fix: use head -1 to handle multiple efibootmgr matches

--- reboot-wrapper.sh
+++ reboot-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Extract the dynamically assigned boot number for Windows Boot Manager
-WIN_NUM=$(efibootmgr | grep "Windows Boot Manager" | cut -c 5-8)
+WIN_NUM=$(efibootmgr | grep "Windows Boot Manager" |head -1| cut -c 5-8)
 
 # Error handling: check if the variable is empty (-z) to prevent breaking the boot
 if [ -z "$WIN_NUM" ]; then
